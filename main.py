f6dec5c3e5edc677e23cf1283d1fae1010398d6c
taokyla <seeyouking@gmail.com>
2023-07-16 02:53:34 +0800

    load env as default

--- main.py
+++ main.py
@@ -5,74 +5,117 @@ import re
 import aria2p
 import feedparser
 import requests
-import yaml
+from pathlib import Path
+from loguru import logger
 
-MAX_HISTORY = 300
+workspace = Path(os.path.realpath(__file__)).parent
 
-workspace = os.path.dirname(os.path.realpath(__file__))
-config_path = os.path.join(workspace, 'config', 'config.yml')
-history_path = os.path.join(workspace, 'config', 'history.txt')
+if env_config_path := os.getenv("MTA_CONFIGPATH"):
+    logger.info(f"load config from env, {env_config_path}")
+    config_path = Path(env_config_path)
+else:
+    logger.info("use default config path, config/config.yml")
+    config_path = workspace.joinpath("config", "config.yml")
 
-torrents_save_dir = os.path.join(workspace, 'torrents')
-if not os.path.exists(torrents_save_dir):
-    os.makedirs(torrents_save_dir)
+config = None
+if config_path.exists() and config_path.is_file():
+    if config_path.suffix in (".yml", ".yaml"):
+        import yaml
 
+        config = yaml.safe_load(config_path.open(encoding="utf8"))
+    elif config_path.suffix == '.json':
+        import json
 
-def load_config():
-    if os.path.exists(config_path):
-        return yaml.safe_load(open(config_path, encoding='utf8'))
+        config = json.load(config_path.open(encoding="utf8"))
     else:
-        raise RuntimeError('config not found!')
+        logger.warning(f"unsupported config file, {config_path.name}")
+if not config:
+    logger.error("config not found!")
+    exit(1)
 
+if history_path := os.getenv("MTA_HISTORY_FILE"):
+    history_path = Path(history_path)
+else:
+    history_path = workspace.joinpath("config", "history.txt")
+logger.info(f"use history file: {history_path.as_posix()}")
+history_path.parent.mkdir(parents=True, exist_ok=True)
 
-config = load_config()
+if torrent_dir := os.getenv("MTA_TORRENTS_DIR"):
+    torrents_save_dir = Path(torrent_dir)
+else:
+    torrents_save_dir = workspace.joinpath("torrents")
+logger.info(f"us torrent dir: {torrents_save_dir.as_posix()}")
+torrents_save_dir.parent.mkdir(parents=True, exist_ok=True)
 
-MAX_HISTORY = max(MAX_HISTORY, len(list(filter(lambda d:d.get('enable',True),config.get('mikan', [])))) * 48)
+MAX_HISTORY = os.getenv("MTA_MAX_HISTORY", max(300, len([filter(lambda d: d.get('enable', True), config.get('mikan', []))]) * 48))
+logger.info(f"max history size, set to {MAX_HISTORY}")
 
-
-def load_history():
-    h = set()
-    if os.path.exists(history_path):
-        with open(history_path, encoding='utf8') as f:
+def load_history() -> set:
+    history = set()
+    if history_path.exists() and history_path.is_file():
+        with history_path.open(encoding='utf8') as f:
             for line in f:
                 bang = line.strip()
                 if bang == '':
                     continue
-                h.add(bang)
-                if len(h) >= MAX_HISTORY:
+                history.add(bang)
+                if len(history) >= MAX_HISTORY:
                     break
     else:
-        w = open(history_path, 'w')
-        w.close()
-    return h
+        history_path.touch(exist_ok=True)
+    return history
 
 
 downloaded_history = load_history()
 
 cache = []
 
-agent = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62"}
+if "aria2" in config:
+    client = aria2p.API(aria2p.Client(**config['aria2']))
+else:
+    client = None
+    if host := os.getenv("MTA_ARIA2_HOST"):
+        if port := os.getenv("MTA_ARIA2_PORT"):
+            client = aria2p.API(aria2p.Client(host=host, port=int(port), secret=os.getenv("MTA_ARIA2_SECRET", "")))
+if not client:
+    logger.error("aria2 not set!")
+    exit(1)
 
-client = aria2p.API(aria2p.Client(**config['aria2']))
-base_dir = client.get_global_options().get('dir')
+from requests.exceptions import ConnectionError
+
+try:
+    ARIA2_BASE_DIR = client.get_global_options().get('dir')
+except ConnectionError:
+    logger.error("aria2 cant connet!")
+    exit(1)
 
 session = requests.session()
-if config.get('proxy'):
-    session.proxies = config['proxy']
-session.headers = agent
+if http_proxy := os.getenv("HTTP_PROXY"):
+    session.proxies.update(http=http_proxy)
+if https_proxy := os.getenv("HTTPS_PROXY"):
+    session.proxies.update(https=https_proxy)
+if proxy := config.get('proxy'):
+    session.proxies.update(proxy)
+
+agent = os.getenv("MTA_USER_AGENT", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.82")
+session.headers = {"user-agent": agent}
 
 
-def aria2(url, dir):
-    if url.startswith("magnet:?xt="):
-        client.add_magnet(url, options={'dir': f'{base_dir}/{dir}'})
+def add_to_aria2(url: str, save_dir: str):
+    logger.info(f"add {url} to aria2")
+    if url.startswith("magnet:?"):
+        client.add_magnet(url, options={'dir': f'{ARIA2_BASE_DIR}/{save_dir}'})
     else:
-        _, filename = os.path.split(url)
-        filename = os.path.join(torrents_save_dir, filename)
-        if not os.path.exists(filename):
+        try:
+            filename = Path(url).name
+        except OSError:
+            filename = url[url.rfind("/") + 1:]
+        filename_path = torrents_save_dir.joinpath(filename)
+        filename_path.parent.mkdir(parents=True, exist_ok=True)
+        if not filename_path.exists():
             resp = session.get(url)
-            with open(filename, 'wb') as w:
-                w.write(resp.content)
-        client.add_torrent(filename, options={'dir': f'{base_dir}/{dir}'})
+            filename_path.write_bytes(resp.content)
+        client.add(filename_path.as_posix(), options={'dir': f'{ARIA2_BASE_DIR}/{save_dir}'})
 
 
 def get_latest(url, rule=None, savedir=None):
@@ -94,7 +137,7 @@ def get_latest(url, rule=None, savedir=None):
                 if link['type'] == 'application/x-bittorrent':
                     download_url = link['href']
             if download_url:
-                aria2(download_url, bangumi_name)
+                add_to_aria2(download_url, bangumi_name)
                 bangumi_cache.add(title)
                 cache.append(title)
         else:
@@ -107,13 +150,15 @@ def write_history(line):
     line = line.strip()
     if line == '':
         return
-    with open(history_path, 'r+', encoding='utf8') as w:
+    with history_path.open(mode='r+', encoding='utf8') as w:
         content = w.read()
         w.seek(0, 0)
         w.write(line + '\n' + content)
 
 
+@logger.catch
 def run():
+    logger.info("mikan to aria2 starting")
     for bangumi in config['mikan']:
         url = bangumi['url']
         rule = bangumi.get('rule')
--- readme.md
+++ readme.md
@@ -7,15 +7,27 @@
 
 - 修改`config/config.default.yml`，重命名为`config.yml`保存到`config`文件夹下
 
+- 或者: 使用环境变量`MTA_CONFIGPATH`指定配置文件，支持yml和json
+
 - 给脚本加个crontab定时执行，例如：
 
 ```
 */30 * * * * python MikanToAria2/main.py
 ```
 
-### config.yml 说明
+## 环境变量
 
-#### aria2
+- `MTA_CONFIGPATH` 配置文件路径，默认为`config/config.yml`
+- `MTA_HISTORY_FILE` 指定历史保存文件路径
+- `MTA_TORRENTS_DIR` 种子保存文件夹，默认为`torrents`
+- `MTA_MAX_HISTORY` 最大加载历史记录，默认为300
+- `MTA_USER_AGENT` 使用的user-agent
+- `HTTP_PROXY` & `HTTPS_PROXY` 代理地址
+- `MTA_ARIA2_HOST`,`MTA_ARIA2_PORT`,`MTA_ARIA2_SECRET` aria2配置
+
+### config.yml 说明 (json也行)
+
+#### aria2 (可选)
 
 - `host` 你的aria2的rpc链接地址
 
@@ -23,12 +35,13 @@
 
 - `secret` rpc密码
 
-#### proxy
+#### proxy (可选)
+
 - `http` 一般是`http://127.0.0.1:1080`，socks5可以用`'socks5://127.0.0.1:1080'`
 
 - `https` 同上，前缀是https,也可以用`'socks5://127.0.0.1:1080'`
 
-#### mikan
+#### mikan `list[dict, ...]`，每项含有以下key组成的字典
 
 - `url` rss订阅链接，例如：https://mikanani.me/RSS/Bangumi?bangumiId=2359&subgroupid=370
 
